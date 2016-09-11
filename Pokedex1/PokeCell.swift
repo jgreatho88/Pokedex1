1bbf737bf5eef1facc1c18ec3d41772b6e0ba658
John Greathouse <johngreathouse@Johns-iMac.local>
2016-09-11 18:10:08 -0500

    loading all pokemon to the screen

---
+++ Pokedex1/PokeCell.swift
@@ -0,0 +1,37 @@
+//
+//  PokeCell.swift
+//  Pokedex1
+//
+//  Created by John Greathouse on 9/11/16.
+//  Copyright © 2016 John Greathouse. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    
+    }
+    
+    
+    
+}
