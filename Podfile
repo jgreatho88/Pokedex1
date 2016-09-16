415878aeb8d5d57457a98526cafaae7ac5512bfd
John Greathouse <johngreathouse@Johns-iMac.local>
2016-09-16 17:23:45 -0500

    added all parameters for the details page

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘9.0’
+use_frameworks!
+
+target 'Pokedex1' do
+pod 'Alamofire', '~> 3.0’
+
+end
---
+++ Pokedex1/Constants.swift
@@ -0,0 +1,42 @@
+//
+//  Constants.swift
+//  Pokedex1
+//
+//  Created by John Greathouse on 9/15/16.
+//  Copyright © 2016 John Greathouse. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+
+let URL_POKEMON = "/api/v1/pokemon/"
+
+
+typealias DownloadComplete = () -> ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
